60796a5c8ac6795f08377906009f311170a4fc35
yahiael <49726084+yahiael@users.noreply.github.com>
2022-12-13 17:27:52 +0100

    added address receiver control

--- receiver-mockup/ses/main.c
+++ receiver-mockup/ses/main.c
@@ -1,15 +1,3 @@
-/* Copyright (c) 2015 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
- */
-
 #include "app_error.h"
 #include "app_pwm.h"
 #include "bsp.h"
@@ -128,10 +116,10 @@ int main(void) {
   config.count_mode = NRF_PWM_MODE_UP,
   config.load_mode = NRF_PWM_LOAD_INDIVIDUAL,
   config.step_mode = NRF_PWM_STEP_AUTO,
-  config.top_value = 2500;
+  config.top_value = T;
   nrf_drv_pwm_init(&m_pwm0, &config, NULL);
 
-  seq_values->channel_0 = 1250;
+  seq_values->channel_0 = T / 2;
   nrf_drv_pwm_simple_playback(&m_pwm0, &seq, 1, NRF_DRV_PWM_FLAG_LOOP);
 
   // Wait indefinitely
@@ -146,7 +134,7 @@ int main(void) {
         rx_buffer[i] = 0;
       }
 
-      /* Activate reception immediately. See NOTE 3 below. */
+      /* Activate reception immediately. */
       dwt_rxenable(DWT_START_RX_IMMEDIATE);
 
       /* Poll until a frame is properly received or an error/timeout occurs. See NOTE 4 below.
@@ -172,8 +160,16 @@ int main(void) {
 
       if (new_data == 1) {
         print_msg(&rx_buffer, frame_len);
-        seq_values->channel_0 = (100 - rx_buffer[1]) * 2500 / 100;
-        nrf_gpio_pin_toggle(PIN_DEBUG);
+        for (int i = 0; i < FRAME_LEN_MAX; i++)
+        {
+          if (rx_buffer[i] == RECEIVER_ADDR)
+          {
+            seq_values->channel_0 = (100 - rx_buffer[i+1]) * T / 100;
+            nrf_gpio_pin_toggle(PIN_DEBUG);
+            break;
+          }
+          
+        }
       }
 
       new_data = 0;
@@ -192,24 +188,3 @@ static void print_msg(uint8 *msg, int n) {
   }
   printf("]\r\n");
 }
-
-/*****************************************************************************************************************************************************
- * NOTES:
- *
- * 1. The single-sided two-way ranging scheme implemented here has to be considered carefully as the accuracy of the distance measured is highly
- *    sensitive to the clock offset error between the devices and the length of the response delay between frames. To achieve the best possible
- *    accuracy, this response delay must be kept as low as possible. In order to do so, 6.8 Mbps data rate is used in this example and the response
- *    delay between frames is defined as low as possible. The user is referred to User Manual for more details about the single-sided two-way ranging
- *    process.  NB:SEE ALSO NOTE 11.
- * 2. The sum of the values is the TX to RX antenna delay, this should be experimentally determined by a calibration process. Here we use a hard coded
- *    value (expected to be a little low so a positive error will be seen on the resultant distance estimate. For a real production application, each
- *    device should have its own antenna delay properly calibrated to get good precision when performing range measurements.
- * 3. This timeout is for complete reception of a frame, i.e. timeout duration must take into account the length of the expected frame. Here the value
- *    is arbitrary but chosen large enough to make sure that there is enough time to receive the complete response frame sent by the responder at the
- *    6.8M data rate used (around 200 s).
- * 4. In a real application, for optimum performance within regulatory limits, it may be necessary to set TX pulse bandwidth and TX power, (using
- *    the dwt_configuretxrf API call) to per device calibrated values saved in the target system or the DW1000 OTP memory.
- * 5. The user is referred to DecaRanging ARM application (distributed with EVK1000 product) for additional practical example of usage, and to the
- *     DW1000 API Guide for more details on the DW1000 driver functions.
- *
- ****************************************************************************************************************************************************/
